203ee46fcb4a4059ee8a5996bf645bfd23249ca0
rodrigo <rodrigogabela@gmail.com>
2025-10-29 17:28:07 +0100

    29/10/2025 Ejercicios clase para entregar

--- Exercices/Boletin/ex2501.py
+++ Exercices/Boletin/ex2501.py
@@ -1,9 +1,10 @@
 import random
 
-def repartoAlumnos (lista, n):
+def repartoAlumnos(lista, n):
     random.shuffle(lista)
-    listas = [lista[(len(lista)//n)*i:(len(lista)//n)+(len(lista)//n)*i] for i in range(n)]
-    return [print('Grupo ', lista) for lista in listas]
+    tamaño = len(lista) // n
+    listas = [lista[tamaño*i : tamaño*(i+1)] for i in range(n)]
+    return [print(f'Grupo {i+1}:', grupo) for i, grupo in enumerate(listas)]
 
 lista = ['a' + str(x + 1) for x in range(50)]
-repartoAlumnos(lista, 5)
+repartoAlumnos(lista, 10)
--- Exercices/Boletin/ex2503.py
+++ Exercices/Boletin/ex2503.py
@@ -2,29 +2,45 @@ from datetime import datetime
 
 club = dict()
 
-def opera2(operador, apodo, fecha=datetime):
-    return {
-        '2': lambda: club.pop(apodo),
-        '4': lambda: [print(v,k) for v, k in club.items()],
-        '5': lambda: [print(v,k) for v, k in club.items()],
-        '6': lambda: [print(k,v) for k, v in club.items() if k[1]>fecha.year],
-        '7': lambda: False
-    }.get(operador, lambda: None)
+def opera2(operador, apodo='', nombre='', fecha=datetime):
+    if operador == '1':
+        club[apodo] = [nombre, datetime.now().strftime('%d/%m/%Y %H:%M:%S')]
+        print(f'Socio {apodo} dado de alta.')
+
+    elif operador == '2':
+        if apodo in club:
+            club.pop(apodo)
+            print(f'Socio {apodo} eliminado.')
+        else:
+            print('Ese apodo no existe.')
+    elif operador == '3':
+        if apodo in club:
+            club[apodo] = [nombre,  datetime.now().strftime('%d/%m/%Y %H:%M:%S')]
+            print(f'Socio {apodo} modificado.')
+        else:
+            print('Ese apodo no existe.')
+    elif operador == '4':
+        [print(v, k) for v, k in club.items()]
+    elif operador == '5':
+        [print(v, k) for v, k in club.items()]
+    elif operador == '6':
+        [print(k, v) for k, v in club.items() if datetime.strptime(v[1], '%d/%m/%Y %H:%M:%S').year < fecha.now().year]
+    elif operador == '7':
+        return False
+    else:
+        print('Opción no válida.')
+    return True
+
 
 a = True
-while a is True:
-    op = input('''1. Alta socio.\n2. Baja socio.\n3. Modificación socio.\n4. Listar socios por apodo.\n5. Listar socios por antigüedad.\n6. Listar los socios con alta anterior a 2025\n7. Salir''')
+while a:
+    op = input('''\n1. Alta socio.\n2. Baja socio.\n3. Modificación socio.\n4. Listar socios por apodo.\n5. Listar socios por antigüedad.\n6. Listar los socios con alta anterior a 2025.\n7. Salir\n> ''')
 
-    apodo = input('Dime tu apodo')
-    nombre = input('Dime tu nombre')
-
-    if op == '1' or op == '3':
-        club[apodo] = [nombre, datetime.now()]
-
-    if op != '7':
-        opera2(op, apodo=apodo)()
+    if op in ('1', '2', '3'):
+        apodo = input('Dime tu apodo: ')
+        nombre = input('Dime tu nombre: ')
     else:
-        a = op
+        apodo = ''
+        nombre = ''
 
-
-
+    a = opera2(op, apodo, nombre)
