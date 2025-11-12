f0d9cf585172b1a5c37c5c102d91c70dfd620c7d
rodrigo <rodrigogabela@gmail.com>
2025-11-12 14:12:04 +0100

    12/11/2025 Boletin POO ex3006

---
+++ Exercices/Boletin_3/ex3006.py
@@ -0,0 +1,23 @@
+class Persona:
+    nombre: str
+    edad: int
+    genero: str
+
+    def __init__(self, nombre, edad, genero):
+        self.nombre = nombre
+        self.edad = edad
+        self.genero = genero
+
+    def presentarse(self):
+        print(f"\nSoy {self.nombre} y tengo {self.edad} con género {self.genero} ")
+
+    def esAdulto(self):
+        if self.edad >= 18:
+            return True
+        else:
+            return False
+
+
+persona = Persona("Juanito", 10 , "Masculino")
+persona.presentarse()
+print("Es adulto" if persona.esAdulto() else "Es menor de edad")
